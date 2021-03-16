c0fb620a5a8f2eee636fe3f755dbde8c0ff45049
Nadav Friedman <NadavFredi@gmail.com>
2021-03-16 03:39:03 +0200

    svg background and css on navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -7,12 +7,15 @@ import { BsFillStarFill } from "react-icons/bs";
 const Navbar = () => {
     return (
         <div className={classes.navbar}>
-            <div className={`Container ${classes.flex}`}>
-                <img src={logo} alt="logo" className={classes.flexItem} />
-                <div className={classes.flexItem}>
-                    <BsFillStarFill className={classes.favoriteLogo} size="1.5rem" color="#e6bc2f" />
+            <div className="Container">
+                <div className={classes.flex}>
+                    <img src={logo} alt="logo" className={classes.flexItem} />
+                    <div className={classes.flexItem}>
+                        <BsFillStarFill className={classes.favoriteLogo} size="1.5rem" color="#e6bc2f" />
                     favorites
                     </div>
+                </div>
+
 
             </div>
         </div>
